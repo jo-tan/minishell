22ec76392e831b4a78041cbf6b14fdfabe598cb4
Jo-hsi TAN <jo-tan@paul-f5Ar5s1.clusters.42paris.fr>
2024-01-02 12:29:43 +0100

    fix env bug with multiple args

--- src/buildin_env.c
+++ src/buildin_env.c
@@ -6,7 +6,7 @@
 /*   By: jo-tan <jo-tan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 09:13:33 by jo-tan            #+#    #+#             */
-/*   Updated: 2024/01/01 09:14:00 by jo-tan           ###   ########.fr       */
+/*   Updated: 2024/01/02 12:28:21 by jo-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ int	ft_env(char **args, t_env *env, int fd)
 
 	p = env;
 	if (args[1] != NULL)
+	{
 		write(2, "env: warning: additional arguments to env are ignored\n", 54);
+		return (127);
+	}
 	while (p)
 	{
 		if (ft_str_contains_char(p->line, '='))
